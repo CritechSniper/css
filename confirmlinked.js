0e86696433076b1399354a8b6b933988f1261073
Waleed Ammar <waleed.gamer173@gmail.com>
2026-03-09 13:48:17 +0400

    Change isFlagFalse to true in confirmlinked.js

--- confirmlinked.js
+++ confirmlinked.js
@@ -1,1 +1,1 @@
-console.log("confirmlinked.js loaded");const isFlagFalse = false;const event = new CustomEvent('confirmlinkedFlag', {detail: { value: isFlagFalse }});document.dispatchEvent(event);
+console.log("confirmlinked.js loaded");const isFlagFalse = true;const event = new CustomEvent('confirmlinkedFlag', {detail: { value: isFlagFalse }});document.dispatchEvent(event);
